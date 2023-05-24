31c369caae1c9b1f77b624e15cf164b5c2ed61ab
vidhijayswal <72997549+vidhijayswal@users.noreply.github.com>
2023-05-23 20:24:55 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  firstday_project
+//
+//  Created by vidhi jayswal on 21/01/19.
+//  Copyright © 2019 vidhi jayswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func hello(_ sender: Any)
+    {
+        print("Hello")
+    }
+    
+    @IBAction func world(_ sender: Any)
+    {
+        print("World")
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
+
